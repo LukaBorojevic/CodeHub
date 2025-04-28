8e97329315bf560a182a239f363e0a4331681392
LukaBorojevic <lukaborojevic97@gmail.com>
2025-04-28 10:23:49 +0200

    array zadatak2

--- arrays/src/Main.java
+++ arrays/src/Main.java
@@ -2,10 +2,10 @@
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {10, 20, 30, 40, 50};
-
+ArrayHelper helper = new ArrayHelper();
         int index = 2;
-        int value = ArrayHelper.getValueAtIndex(numbers, index);
-        System.out.println("Value at index " + index + " is: " + value);
+        int value = helper.getValueAtIndex(numbers, index);
+       
     }
 }
 
