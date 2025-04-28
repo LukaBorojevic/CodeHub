69075eb856b7a14332bbde88256cea1e085c5c2d
LukaBorojevic <lukaborojevic97@gmail.com>
2025-04-28 10:34:32 +0200

    commit

--- arrays/src/ArrayHelper.java
+++ arrays/src/ArrayHelper.java
@@ -10,4 +10,14 @@ public class ArrayHelper {
     public int getValueAtIndex(int[] array, int index) {
         return array[index];
     }
+    public int calculateSum(int[] array) {
+        int a = 0;
+        for (int i = 0; i < array.length; i++) {
+            a = a + array[i];
+        }
+        return a;
+    }
+    public void print(int value){
+        System.out.println(value);
+    }
 }
